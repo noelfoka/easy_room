fc7a8d64c8e7e85c007827019efa257c700329ea
noelfoka <nomgnenoel@gmail.com>
2025-01-16 11:41:51 +0100

    ajout du second titre

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,14 @@ const page = () => {
         <div className='relative hero-content text-center mx-4 lg:mx-[15%] py-20'>
 
           <div className="max-w-lg">
-            <h1>
+            <h1 className='text-3xl md:text-7xl flex justify-center items-center'>
               <div className='bg-secondary p-1 mr-1 rounded-md text-white'>
               <CalendarCheck className='w-8 h-8 md:w-16 md:h-16' />
               </div>
-              <span>Easy<span className="text-primary">Room</span></span>
+              <span>Es<span className="text-secondary">Rom</span></span>
             </h1>
+
+            <h2 className='py-4 md:py-6 text-xl md:text-3xl font-semibold'>Gérez la réservation de vos <span>salles de réunion</span> en toute simplicité</h2>
           </div>
 
         </div>
