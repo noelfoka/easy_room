96bef18b7fb7abe31129c70d9ab796e44cc29482
noelfoka <nomgnenoel@gmail.com>
2025-01-16 19:42:03 +0100

    recuperation de user et pathname

--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { usePathname } from 'next/navigation'
 
 type WrapperProps = {
   children: React.ReactNode
 }
 
 const Wrapper = ({children}: WrapperProps) => {
+
+  const {user} = useKindeBrowserClient();
+  const pathname = usePathname();
+
   return (
     <div>
       <Navbar />
-      <div>{children}</div>
+      <div className='px-5 mt-32 md:px-[10%] mb-10'>{children}</div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="winter">
+    <html lang="en" data-theme="cmyk">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
