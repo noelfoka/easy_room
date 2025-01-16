d9d694a4eb8c4cd04750101cb6b77df3c5aa1750
noelfoka <nomgnenoel@gmail.com>
2025-01-16 16:54:05 +0100

    Api permettant d'enregistrer un nouvel utilisateur dans la base de données

---
+++ app/api/users/route.ts
@@ -0,0 +1,8 @@
+// Api permettant d'enregistrer un nouvel utilisateur dans la base de données
+
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  
+}
