bc2c460528af971befdc6d50743f254775c01aea
noelfoka <nomgnenoel@gmail.com>
2025-01-16 16:58:58 +0100

    Récupération des données de l'utilisateur

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,5 +4,16 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function POST(request: Request) {
-  
+
+  try {
+    // Récupération des données de l'utilisateur
+    const { email, familyName, givenName } = await request.json();
+  } catch (error) {
+    console.error('Erreur api users', error);
+    return NextResponse.json(
+      { message: "Une erreur est survenue lors de l'enregistrement de l'utilisateur" },
+      { status: 500 }
+    )
+  }
+
 }
