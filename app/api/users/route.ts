e3b97f8f0390736c16f3224aadfd2b0b2879a023
noelfoka <nomgnenoel@gmail.com>
2025-01-16 17:09:35 +0100

    Vérifier si les champs requis sont présents

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,6 +8,15 @@ export async function POST(request: Request) {
   try {
     // Récupération des données de l'utilisateur
     const { email, familyName, givenName } = await request.json();
+
+    // Vérifier si les champs requis sont présents
+    if (!email || !familyName || !givenName) {
+      return NextResponse.json(
+        { message: "Les champs 'email', 'familyName' et 'givenName' sont requis" },
+        { status: 400 }
+      )
+    }
+
   } catch (error) {
     console.error('Erreur api users', error);
     return NextResponse.json(
