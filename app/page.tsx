724459088210aae21f02d6ea217881ea7f8ab2aa
noelfoka <nomgnenoel@gmail.com>
2025-01-16 12:54:04 +0100

    ajout de image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { CalendarCheck } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
     <div className="relative">
-      <section className="w-full">
+      <section
+        className="w-full"
+        style={{
+          backgroundColor: "#ffffff",
+          backgroundImage: "url('/img.jpg')",
+        }}
+      >
         <div className="relative hero-content text-center mx-4 lg:mx-[15%] py-20">
           <div className="max-w-lg">
             <h1 className="text-3xl md:text-7xl flex justify-center items-center">
@@ -17,20 +24,39 @@ const page = () => {
             </h1>
 
             <h2 className="py-4 md:py-6 text-xl md:text-3xl font-semibold">
-              Gérez la réservation de vos <span className="text-secondary">salles de réunion</span> en
-              toute simplicité
+              Gérez la réservation de vos{" "}
+              <span className="text-secondary">salles de réunion</span> en toute
+              simplicité
             </h2>
 
             <ul className="steps steps-vertical lg:steps-horizontal">
-              <li className="step step-secondary text-sm"><div className="badge">Créer un compte</div></li>
-              <li className="step step-secondary text-sm"><div className="badge">Réservez vos salles</div></li>
-              <li className="step step-secondary text-sm"><div className="badge">Créer vos réservations</div></li>
+              <li className="step step-secondary text-sm">
+                <div className="badge">Créer un compte</div>
+              </li>
+              <li className="step step-secondary text-sm">
+                <div className="badge">Réservez vos salles</div>
+              </li>
+              <li className="step step-secondary text-sm">
+                <div className="badge">Créer vos réservations</div>
+              </li>
             </ul>
 
             <div className="mt-6 flex justify-center">
               <button className="btn btn-secondary"> Se connecter</button>
-              <button className="btn btn-secondary btn-outline ml-4"> S&apos;inscrire</button>
+              <button className="btn btn-secondary btn-outline ml-4">
+                {" "}
+                S&apos;inscrire
+              </button>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex justify-center items-center">
+
+        <div className="p-5 md:p-0">
+          <div>
+            <Image src="/demo.png" alt="demo" width={1600} height={500} />
           </div>
         </div>
       </section>
