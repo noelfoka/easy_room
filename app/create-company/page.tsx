4fb10c13639bfad96fd1bb81651028e3d790d6b7
noelfoka <nomgnenoel@gmail.com>
2025-01-17 12:14:22 +0100

    bouton de creation d'entreprise

--- app/create-company/page.tsx
+++ app/create-company/page.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Wrapper from '../components/Wrapper'
+import React from "react";
+import Wrapper from "../components/Wrapper";
 
 const page = () => {
   return (
     <Wrapper>
-    <div>
-      <h1 className='text-2xl mb-4'>Créer une entreprise</h1>
-      <form>
-        <div className='flex flex-row'>
-          <input type="text" name="companyName" id="companyName" placeholder='Nom de l&apos;entreprise' required className='input input-bordered w-full max-w-xs' />
-        </div>
-      </form>
-    </div>
-    </Wrapper>
-  )
-}
+      <div>
+        <h1 className="text-2xl mb-4">Créer une entreprise</h1>
+        <form>
+          <div className="flex flex-row">
+            <input
+              type="text"
+              name="companyName"
+              id="companyName"
+              placeholder="Nom de l'entreprise"
+              required
+              className="input input-bordered w-full max-w-xs"
+            />
 
-export default page
+            <button type="submit" className="btn btn-secondary ml-2">Creer l&apos;entreprise</button>
+          </div>
+        </form>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default page;
