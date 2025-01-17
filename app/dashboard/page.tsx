7fb0f684db5bc430c765d98d6238e9f4fb59e022
noelfoka <nomgnenoel@gmail.com>
2025-01-17 05:59:54 +0100

    fetchCompanyId

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Wrapper from "../components/Wrapper";
 
 const page = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { user } = useKindeBrowserClient();
+  const [companyId, setCompanyId] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchCompanyId = async () => {}
 
   return (
     <Wrapper>
       <div>
-        <div>
-          Bienvenue {user?.given_name} {user?.family_name} à mon dashboard
-        </div>
+        
       </div>
     </Wrapper>
   );
