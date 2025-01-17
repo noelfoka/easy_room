7fc4c0c6a71b92c56d29633903c7485eb784bf85
noelfoka <nomgnenoel@gmail.com>
2025-01-17 06:35:06 +0100

    prochaine etape

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
@@ -10,40 +10,39 @@ const page = () => {
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchCompanyId = async () => {
-   // Vérifier si il y a un user connecté
-   if (user) {
-    try {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: user.email,
-          familyName: user.family_name,
-          givenName: user.given_name
-        })
-      });
+    // Vérifier si il y a un user connecté
+    if (user) {
+      try {
+        const response = await fetch("/api/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: user.email,
+            familyName: user.family_name,
+            givenName: user.given_name,
+          }),
+        });
 
-      // Récupérer les données de l'utilisateur
-      const data = await response.json();
+        // Récupérer les données de l'utilisateur
+        const data = await response.json();
 
-      // Mettre à jour le companyId
-      setCompanyId(data.companyId || null);
-      setLoading(false);
-
-    } catch (error) {
-      console.error(error);
-      setCompanyId(null);
+        // Mettre à jour le companyId
+        setCompanyId(data.companyId || null);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setCompanyId(null);
+      }
     }
-   }
-  }
+  };
 
   // Appel de la fonction fetchCompanyId au chargement de la page
   useEffect(() => {
     const initializeData = async () => {
       await fetchCompanyId();
-    }
+    };
     initializeData();
   }, [user]);
 
@@ -51,16 +50,20 @@ const page = () => {
     return (
       <Wrapper>
         <div className="w-full flex justify-center">
-        <span className="loading loading-spinner loading-lg"></span>
+          <span className="loading loading-spinner loading-lg"></span>
         </div>
       </Wrapper>
-    )
+    );
   }
 
   return (
     <Wrapper>
       <div>
-        
+        <div>
+          <div className="badge badge-secondary badge-outline">
+            Company ID: {companyId}
+          </div>
+        </div>
       </div>
     </Wrapper>
   );
