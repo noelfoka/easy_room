1d8a2c2c9a55ed53cee436e59274d542a9c44221
noelfoka <nomgnenoel@gmail.com>
2025-01-17 07:07:26 +0100

    Vérifier si les champs requis sont présents

--- app/api/companies/route.ts
+++ app/api/companies/route.ts
@@ -7,7 +7,17 @@ export async function POST(request: Request) {
   try {
     // extraction des données de la requête
     const { email, companyName } = await request.json();
+
+    // Vérifier si les champs requis sont présents
+    if (!email || !companyName) {
+      return NextResponse.json(
+        { message: "Les champs 'email' et 'companyName' sont requis" },
+        { status: 400 }
+      )
+    }
+
   } catch (error) {
+    console.error('Erreur api companies', error);
     return NextResponse.json(
       { message: "Une erreur est survenue lors de la création de la company" },
       { status: 500 }
