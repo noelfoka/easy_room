56c9e9458ebdb003b1bea371ccf2b4d8415d5131
noelfoka <nomgnenoel@gmail.com>
2025-01-17 14:52:33 +0100

    onclose

--- app/components/Notification.tsx
+++ app/components/Notification.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import { Info } from "lucide-react";
+import React, { useEffect } from "react";
 
 interface NotificationProps {
-  message: string
-  onClose: () => void
+  message: string;
+  onClose: () => void;
 }
 
-const Notification = () => {
+const Notification: React.FC<NotificationProps> = ({ message, onClose }) => {
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
   return (
-    <div>Notification</div>
-  )
-}
+    <div className="toast toasr-top-left">
+      <div className="alert p-2 text-sm shadow-lg border boeder-secondary rounded-md">
+        <span className="flex items-center">
+          <Info className="w-4 mr-2 font-bold text-secondary" />
+          {message}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Notification
+export default Notification;
