a41f6ff5ebe55dcf2a4c411b79dd69a694c13509
noelfoka <nomgnenoel@gmail.com>
2025-01-17 07:04:59 +0100

    extraction des données de la requête

--- app/api/companies/route.ts
+++ app/api/companies/route.ts
@@ -5,7 +5,8 @@ import prisma from "@/prisma/lib/prisma";
 
 export async function POST(request: Request) {
   try {
-    
+    // extraction des données de la requête
+    const { email, companyName } = await request.json();
   } catch (error) {
     return NextResponse.json(
       { message: "Une erreur est survenue lors de la création de la company" },
