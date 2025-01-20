8ae14610240c28da37432e86c4272150e6619283
noelfoka <nomgnenoel@gmail.com>
2025-01-20 16:23:15 +0100

    recuperation de l'utilisateur

--- app/employees/[companyId]/page.tsx
+++ app/employees/[companyId]/page.tsx
@@ -1,6 +1,10 @@
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import React from 'react'
 
 const page = ({params}: {params: {companyId: string}}) => {
+
+  const { user } = useKindeBrowserClient();
+
   return (
     <div>
       {params.companyId}
