7512d27796916bd34440463af7ef618504f2421a
noelfoka <nomgnenoel@gmail.com>
2025-01-20 16:32:55 +0100

    wrapper

--- app/employees/[companyId]/page.tsx
+++ app/employees/[companyId]/page.tsx
@@ -1,14 +1,39 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import Wrapper from '@/app/components/Wrapper';
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
-import React from 'react'
+import React, { useState } from 'react'
+import Notification from '@/app/components/Notification';
 
 const page = ({params}: {params: {companyId: string}}) => {
 
   const { user } = useKindeBrowserClient();
+  const [employeeEmail, setEmployeeEmail] = useState<string>("");
+  const [companyName, setCompanyName] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [notification, setNotification] = useState<string>("");
+  const closeNotification = () => {
+    setNotification("");
+  };
 
   return (
-    <div>
-      {params.companyId}
-    </div>
+    <Wrapper>
+      {notification && (
+        <Notification
+          message={notification}
+          onClose={closeNotification}
+        ></Notification>
+      )}
+
+      <div>
+        {loading ? (
+          <div className="text-center mt-32">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
+        ) : ()}
+      </div>
+
+    </Wrapper>
   )
 }
 
