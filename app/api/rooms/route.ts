2cccda83a758df95ad536ccb7ccb54062813f372
noelfoka <nomgnenoel@gmail.com>
2025-01-22 16:25:39 +0100

    Vérifier que roomId et imgUrl sont présents

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -5,13 +5,12 @@ import prisma from "@/prisma/lib/prisma";
 
 export async function POST(request: Request) {
   try {
-
     // extraction des données de la requête
-    const {action, name, capacity, description, imgUrl, companyId} = await request.json();
+    const { action, name, capacity, description, imgUrl, companyId, roomId } =
+      await request.json();
 
     // définir l'action
     if (action === "SAVE_DATA") {
-
       // Vérification de certaines informations
       if (!name || !capacity || !companyId) {
         return NextResponse.json(
@@ -24,19 +23,43 @@ export async function POST(request: Request) {
       const existingRoom = await prisma.room.findFirst({
         where: {
           name: name,
-          companyId: companyId
-        }
-      })
+          companyId: companyId,
+        },
+      });
 
       if (existingRoom) {
         return NextResponse.json(
-          { message: "Cette salle existe déjà" },
+          { message: "Cette salle existe déjà pour cette entreprise" },
+          { status: 409 }
+        );
+      }
+
+      // Creer la salle
+      const newRoom = await prisma.room.create({
+        data: {
+          name,
+          capacity: parseInt(capacity, 10),
+          description,
+          company: { connect: { id: companyId } },
+        },
+      });
+
+      return NextResponse.json(
+        { message: "Salle créee avec succès", roomId: newRoom.id },
+        { status: 201 }
+      );
+    } else if (action === "SAVE_IMAGE") {
+      // Enregistrer l'image
+
+      // Vérifier que roomId et imgUrl sont présents
+      if (!roomId || !imgUrl) {
+        return NextResponse.json(
+          { message: "roomId et imgUrl sont obligatoires" },
           { status: 400 }
         );
       }
 
-    } else if (action === "SAVE_IMAGE") {}
-
+    }
   } catch (error) {
     console.error("Erreur api rooms", error);
     return NextResponse.json(
