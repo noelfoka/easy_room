708a6c1a6c91bfbedd171067836f5cf8dad6902a
noelfoka <nomgnenoel@gmail.com>
2025-01-22 14:21:03 +0100

    extraction des données de la requête

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -5,7 +5,9 @@ import prisma from "@/prisma/lib/prisma";
 
 export async function POST(request: Request) {
   try {
-    
+
+    // extraction des données de la requête
+    const {action, name, capacity, description, imgUrl, companyId} = await request.json();
   } catch (error) {
     console.error("Erreur api rooms", error);
     return NextResponse.json(
