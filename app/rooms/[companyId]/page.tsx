904598e38af4b93e2c72b802aeded785045d6075
noelfoka <nomgnenoel@gmail.com>
2025-01-22 17:50:34 +0100

    premier input

--- app/rooms/[companyId]/page.tsx
+++ app/rooms/[companyId]/page.tsx
@@ -20,10 +20,10 @@ const page = ({ params }: { params: { companyId: string } }) => {
         ></Notification>
       )}
 
-      <div>{params.companyId}</div>
-
       <section className="w-[450px] ml-8 hidden md:block">
         <h1 className="text-2xl mb-4">Créer une salle</h1>
+
+        <input type="text"  placeholder="Nom de la salle" className="input input-bordered w-full max-w-xs mb-4" />
       </section>
 
     </Wrapper>
