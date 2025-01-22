81024b63b34369469ae627472bd088ae92cdeca7
noelfoka <nomgnenoel@gmail.com>
2025-01-22 14:17:18 +0100

    try catch post

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -2,3 +2,15 @@
 
 import { NextResponse } from "next/server";
 import prisma from "@/prisma/lib/prisma";
+
+export async function POST(request: Request) {
+  try {
+    
+  } catch (error) {
+    console.error("Erreur api rooms", error);
+    return NextResponse.json(
+      { message: "Une erreur est survenue lors de la création de la salle" },
+      { status: 500 }
+    );
+  }
+}
