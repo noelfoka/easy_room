c5f7daca58d887f701d8b7f7cf2b87ec327e2adc
noelfoka <nomgnenoel@gmail.com>
2025-01-22 06:06:11 +0100

    Extraire l'id de la company à récupérer

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -4,6 +4,10 @@ import prisma from "@/prisma/lib/prisma";
 // Api permettant de récupérer les employés d'une entreprise
 export async function GET(request: Request) {
   try {
+
+    // Extraire l'id de la company à récupérer
+    const searchParams = new URL(request.url);
+    const companyId = searchParams.get("companyId");
     
   } catch (error) {
     console.error("Erreur api employees", error);
