c15ad6e2040ff2d40881443b465826301ad7aebd
noelfoka <nomgnenoel@gmail.com>
2025-01-22 06:02:13 +0100

    Une erreur est survenue lors de la récupération des employés

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -2,3 +2,15 @@ import { NextResponse } from "next/server";
 import prisma from "@/prisma/lib/prisma";
 
 // Api permettant de récupérer les employés d'une entreprise
+export async function GET(request: Request) {
+  try {
+    
+  } catch (error) {
+    console.error("Erreur api employees", error);
+    return NextResponse.json(
+      { message: "Une erreur est survenue lors de la récupération des employés" },
+      { status: 500 }
+    );
+    
+  }
+}
