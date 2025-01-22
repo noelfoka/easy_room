8124e99aae54e56bdef2a3c8fedaafdadbd4bc30
noelfoka <nomgnenoel@gmail.com>
2025-01-22 18:02:54 +0100

    un deuxieme input et un textarea

--- app/rooms/[companyId]/page.tsx
+++ app/rooms/[companyId]/page.tsx
@@ -6,9 +6,11 @@ import React, { useState } from "react";
 import Notification from "@/app/components/Notification";
 
 const page = ({ params }: { params: { companyId: string } }) => {
-
   // Variales d'etat
   const [file, setFile] = useState<File | null>(null);
+  const [name, setName] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [description, setDescription] = useState("");
 
   const [notification, setNotification] = useState<string>("");
   const closeNotification = () => {
@@ -27,9 +29,25 @@ const page = ({ params }: { params: { companyId: string } }) => {
       <section className="w-[450px] ml-8 hidden md:block">
         <h1 className="text-2xl mb-4">Créer une salle</h1>
 
-        <input type="text"  placeholder="Nom de la salle" className="input input-bordered w-full max-w-xs mb-4" />
-      </section>
+        <input
+          type="text"
+          value={name}
+          placeholder="Nom de la salle"
+          className="input input-bordered w-full max-w-xs mb-4"
+          onChange={(e) => setName(e.target.value)}
+        />
 
+        <input
+          type="number"
+          value={capacity}
+          placeholder="Capacité de la salle"
+          className="input input-bordered w-full max-w-xs mb-4"
+          onChange={(e) => setCapacity(e.target.value)}
+        />
+
+        <textarea value={description} placeholder="Description de la salle" className="textarea textarea-bordered w-full max-w-xs mb-4" onChange={(e) => setDescription(e.target.value)}></textarea>
+
+      </section>
     </Wrapper>
   );
 };
