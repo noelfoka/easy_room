8ec7a52afcc15ab879d3e3b54458705f6545b9ec
noelfoka <nomgnenoel@gmail.com>
2025-01-22 12:56:07 +0100

    ajout des params

--- app/rooms/[companyId]/page.tsx
+++ app/rooms/[companyId]/page.tsx
@@ -1,8 +1,10 @@
+"use client"
+
 import React from 'react'
 
-const page = () => {
+const page = ({ params }: { params: { companyId: string } }) => {
   return (
-    <div>page</div>
+    <div>{params.companyId}</div>
   )
 }
 
