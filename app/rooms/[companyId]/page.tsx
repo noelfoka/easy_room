0fd2a5a638043e3a082c53d95da543db3702c434
noelfoka <nomgnenoel@gmail.com>
2025-01-23 10:05:06 +0100

    popup modal

--- app/rooms/[companyId]/page.tsx
+++ app/rooms/[companyId]/page.tsx
@@ -9,10 +9,9 @@ import FileUpload from "@/app/components/FileUpload";
 import { useEdgeStore } from "@/lib/edgestore";
 
 const page = ({ params }: { params: { companyId: string } }) => {
-
   const edgestore = useEdgeStore();
 
-    const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   // Variales d'etat
   const [file, setFile] = useState<File | null>(null);
@@ -28,16 +27,15 @@ const page = ({ params }: { params: { companyId: string } }) => {
 
   const handleFileChange = (selectedFile: File | null) => {
     setFile(selectedFile);
-  }
+  };
 
   const handleUpload = async () => {
-    if(!name || !capacity || !description) {
+    if (!name || !capacity || !description) {
       setNotification("Les informations sont incomplètes");
       return;
     }
-    
-    try {
 
+    try {
       // Vérifier si la creation de la salle est possible
       const apiResponse = await fetch("/api/rooms", {
         method: "POST",
@@ -49,15 +47,15 @@ const page = ({ params }: { params: { companyId: string } }) => {
           name,
           capacity,
           description: description,
-          imgUrl: '',
+          imgUrl: "",
           companyId: params.companyId,
         }),
-      })
+      });
 
       // Si la reponse est  bonne
       if (apiResponse.ok) {
         const room = await apiResponse.json();
-        
+
         let imageUploadSuccess = false;
         if (file) {
           const res = await edgestore.edgestore.publicFiles.upload({
@@ -65,7 +63,7 @@ const page = ({ params }: { params: { companyId: string } }) => {
             onProgressChange: (progress) => {
               setProgress(progress);
             },
-          })
+          });
           console.log("file uploaded", res.url);
 
           const imageResponse = await fetch("/api/rooms", {
@@ -94,9 +92,10 @@ const page = ({ params }: { params: { companyId: string } }) => {
         // Si l'image a été uploadée
         if (imageUploadSuccess) {
           setNotification("Salle créée avec succès et image uploadée");
-
-        }else {
-          setNotification("Salle créée avec succès mais l'image n'a pas pu être uploadée");
+        } else {
+          setNotification(
+            "Salle créée avec succès mais l'image n'a pas pu être uploadée"
+          );
         }
 
         setName("");
@@ -105,19 +104,20 @@ const page = ({ params }: { params: { companyId: string } }) => {
         setProgress(0);
         setFile(null);
 
-        if(fileInputRef.current) {
-          fileInputRef.current.value = '';
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
         }
       } else {
         const result = await apiResponse.json();
         setNotification(`${result.message}`);
       }
-      
     } catch (error) {
       console.error(error);
-      setNotification("Une erreur est survenue lors de la création de la salle");
+      setNotification(
+        "Une erreur est survenue lors de la création de la salle"
+      );
     }
-  }
+  };
 
   return (
     <Wrapper>
@@ -127,6 +127,26 @@ const page = ({ params }: { params: { companyId: string } }) => {
           onClose={closeNotification}
         ></Notification>
       )}
+
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => (document.getElementById("my_modal") as HTMLDialogElement).showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
 
       <section className="w-[450px] ml-8 hidden md:block">
         <h1 className="text-2xl mb-4">Créer une salle</h1>
@@ -169,16 +189,25 @@ const page = ({ params }: { params: { companyId: string } }) => {
               <p className="border border-base-300 p-3 mt-4 rounded-lg">
                 <span className="text-sm">{file.name}</span>
                 {progress > 0 && (
-                <p>
-                  <progress className="progress progress-secondary w-56" value={progress} max="100"></progress>
-                  <span className="text-sm">{progress}%</span>
-                </p>
-              )}
+                  <p>
+                    <progress
+                      className="progress progress-secondary w-56"
+                      value={progress}
+                      max="100"
+                    ></progress>
+                    <span className="text-sm">{progress}%</span>
+                  </p>
+                )}
               </p>
             )}
           </div>
         </div>
-        <button className="btn btn-secondary btn-outline btn-sm mt-4" onClick={handleUpload}>Créer une salle</button>
+        <button
+          className="btn btn-secondary btn-outline btn-sm mt-4"
+          onClick={handleUpload}
+        >
+          Créer une salle
+        </button>
       </section>
     </Wrapper>
   );
