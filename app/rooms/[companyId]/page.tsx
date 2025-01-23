9f6dc27696f20e0c753a5efc48d57429fd7b5237
noelfoka <nomgnenoel@gmail.com>
2025-01-23 11:49:38 +0100

    try catch

--- app/rooms/[companyId]/page.tsx
+++ app/rooms/[companyId]/page.tsx
@@ -19,6 +19,7 @@ const page = ({ params }: { params: { companyId: string } }) => {
   const [capacity, setCapacity] = useState("");
   const [description, setDescription] = useState("");
   const [progress, setProgress] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const [notification, setNotification] = useState<string>("");
   const closeNotification = () => {
@@ -119,6 +120,17 @@ const page = ({ params }: { params: { companyId: string } }) => {
     }
   };
 
+  const fetchRooms = async () => {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      setNotification(
+        "Une erreur est survenue lors de la récupération des salles"
+      );
+    }
+  }
+
   return (
     <Wrapper>
       {notification && (
@@ -128,148 +140,159 @@ const page = ({ params }: { params: { companyId: string } }) => {
         ></Notification>
       )}
 
-      {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <button
-        className="btn my-2 btn-outline btn-secondary w-fit flex md:hidden"
-        onClick={() =>
-          (document.getElementById("my_modal") as HTMLDialogElement).showModal()
-        }
-      >
-        Creer salle
-      </button>
-      <dialog id="my_modal" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-          <section className="w-full">
-            <h1 className="text-2xl mb-4">Créer une salle</h1>
 
-            <input
-              type="text"
-              value={name}
-              placeholder="Nom de la salle"
-              className="input input-bordered w-full mb-4"
-              onChange={(e) => setName(e.target.value)}
-            />
-
-            <input
-              type="number"
-              value={capacity}
-              placeholder="Capacité de la salle"
-              className="input input-bordered w-full mb-4"
-              onChange={(e) => setCapacity(e.target.value)}
-            />
-
-            <textarea
-              value={description}
-              placeholder="Description de la salle"
-              className="textarea textarea-bordered w-full mb-4"
-              onChange={(e) => setDescription(e.target.value)}
-            ></textarea>
-
-            {/* Uploader un fichier */}
-            <div>
-              <div className="p-5 rounded-lg bg-secondary/5 border border-base-300">
-                {/* file upload */}
-                <FileUpload
-                  onFileChange={handleFileChange}
-                  accept="image/*"
-                  buttonLabel="Uploader une image"
-                />
-
-                {/* Afficher le fichier uploadé */}
-                {file && (
-                  <p className="border border-base-300 p-3 mt-4 rounded-lg">
-                    <span className="text-sm">{file.name}</span>
-                    {progress > 0 && (
-                      <p>
-                        <progress
-                          className="progress progress-secondary w-56"
-                          value={progress}
-                          max="100"
-                        ></progress>
-                        <span className="text-sm">{progress}%</span>
-                      </p>
-                    )}
-                  </p>
-                )}
-              </div>
-            </div>
-            <button
-              className="btn btn-secondary btn-outline btn-sm mt-4"
-              onClick={handleUpload}
-            >
-              Créer une salle
-            </button>
-          </section>
-        </div>
-      </dialog>
-
-      <section className="w-[450px] ml-8 hidden md:block">
-        <h1 className="text-2xl mb-4">Créer une salle</h1>
-
-        <input
-          type="text"
-          value={name}
-          placeholder="Nom de la salle"
-          className="input input-bordered w-full mb-4"
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <input
-          type="number"
-          value={capacity}
-          placeholder="Capacité de la salle"
-          className="input input-bordered w-full mb-4"
-          onChange={(e) => setCapacity(e.target.value)}
-        />
-
-        <textarea
-          value={description}
-          placeholder="Description de la salle"
-          className="textarea textarea-bordered w-full mb-4"
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-
-        {/* Uploader un fichier */}
-        <div>
-          <div className="p-5 rounded-lg bg-secondary/5 border border-base-300">
-            {/* file upload */}
-            <FileUpload
-              onFileChange={handleFileChange}
-              accept="image/*"
-              buttonLabel="Uploader une image"
-            />
-
-            {/* Afficher le fichier uploadé */}
-            {file && (
-              <p className="border border-base-300 p-3 mt-4 rounded-lg">
-                <span className="text-sm">{file.name}</span>
-                {progress > 0 && (
-                  <p>
-                    <progress
-                      className="progress progress-secondary w-56"
-                      value={progress}
-                      max="100"
-                    ></progress>
-                    <span className="text-sm">{progress}%</span>
-                  </p>
-                )}
-              </p>
-            )}
+        {loading ? (
+          <div className="text-center mt-32">
+            <span className="loading loading-spinner loading-lg"></span>
           </div>
-        </div>
-        <button
-          className="btn btn-secondary btn-outline btn-sm mt-4"
-          onClick={handleUpload}
-        >
-          Créer une salle
-        </button>
-      </section>
+        ) : (
+          <div>
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <button
+              className="btn my-2 btn-outline btn-secondary w-fit flex md:hidden"
+              onClick={() =>
+                (
+                  document.getElementById("my_modal") as HTMLDialogElement
+                ).showModal()
+              }
+            >
+              Creer salle
+            </button>
+            <dialog id="my_modal" className="modal">
+              <div className="modal-box">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                    ✕
+                  </button>
+                </form>
+                <section className="w-full">
+                  <h1 className="text-2xl mb-4">Créer une salle</h1>
+
+                  <input
+                    type="text"
+                    value={name}
+                    placeholder="Nom de la salle"
+                    className="input input-bordered w-full mb-4"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+
+                  <input
+                    type="number"
+                    value={capacity}
+                    placeholder="Capacité de la salle"
+                    className="input input-bordered w-full mb-4"
+                    onChange={(e) => setCapacity(e.target.value)}
+                  />
+
+                  <textarea
+                    value={description}
+                    placeholder="Description de la salle"
+                    className="textarea textarea-bordered w-full mb-4"
+                    onChange={(e) => setDescription(e.target.value)}
+                  ></textarea>
+
+                  {/* Uploader un fichier */}
+                  <div>
+                    <div className="p-5 rounded-lg bg-secondary/5 border border-base-300">
+                      {/* file upload */}
+                      <FileUpload
+                        onFileChange={handleFileChange}
+                        accept="image/*"
+                        buttonLabel="Uploader une image"
+                      />
+
+                      {/* Afficher le fichier uploadé */}
+                      {file && (
+                        <p className="border border-base-300 p-3 mt-4 rounded-lg">
+                          <span className="text-sm">{file.name}</span>
+                          {progress > 0 && (
+                            <p>
+                              <progress
+                                className="progress progress-secondary w-56"
+                                value={progress}
+                                max="100"
+                              ></progress>
+                              <span className="text-sm">{progress}%</span>
+                            </p>
+                          )}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <button
+                    className="btn btn-secondary btn-outline btn-sm mt-4"
+                    onClick={handleUpload}
+                  >
+                    Créer une salle
+                  </button>
+                </section>
+              </div>
+            </dialog>
+
+            <section className="w-[450px] ml-8 hidden md:block">
+              <h1 className="text-2xl mb-4">Créer une salle</h1>
+
+              <input
+                type="text"
+                value={name}
+                placeholder="Nom de la salle"
+                className="input input-bordered w-full mb-4"
+                onChange={(e) => setName(e.target.value)}
+              />
+
+              <input
+                type="number"
+                value={capacity}
+                placeholder="Capacité de la salle"
+                className="input input-bordered w-full mb-4"
+                onChange={(e) => setCapacity(e.target.value)}
+              />
+
+              <textarea
+                value={description}
+                placeholder="Description de la salle"
+                className="textarea textarea-bordered w-full mb-4"
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+
+              {/* Uploader un fichier */}
+              <div>
+                <div className="p-5 rounded-lg bg-secondary/5 border border-base-300">
+                  {/* file upload */}
+                  <FileUpload
+                    onFileChange={handleFileChange}
+                    accept="image/*"
+                    buttonLabel="Uploader une image"
+                  />
+
+                  {/* Afficher le fichier uploadé */}
+                  {file && (
+                    <p className="border border-base-300 p-3 mt-4 rounded-lg">
+                      <span className="text-sm">{file.name}</span>
+                      {progress > 0 && (
+                        <p>
+                          <progress
+                            className="progress progress-secondary w-56"
+                            value={progress}
+                            max="100"
+                          ></progress>
+                          <span className="text-sm">{progress}%</span>
+                        </p>
+                      )}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <button
+                className="btn btn-secondary btn-outline btn-sm mt-4"
+                onClick={handleUpload}
+              >
+                Créer une salle
+              </button>
+            </section>
+          </div>
+        )}
     </Wrapper>
   );
 };
