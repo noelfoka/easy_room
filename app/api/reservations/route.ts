0b3cd4b58cbb5ee495641824cb4270b8135c7cfd
noelfoka <nomgnenoel@gmail.com>
2025-01-29 23:21:10 +0100

    try catch de reservation

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -1,3 +1,22 @@
 // Api route pour les reservations
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+
+interface ReservationRequest {
+  email: string;
+  roomId: string;
+  reservationDate: string;
+  timeSlots: string [];
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    
+  } catch (error) {
+    console.error("Erreur api reservations", error);
+    return NextResponse.json(
+      { message: "Une erreur est survenue lors de la création de la reservation" },
+      { status: 500 }
+    );
+  }
+}
