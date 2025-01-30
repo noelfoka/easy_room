22538544e8860292bd67554cf05e376a68434bd5
noelfoka <nomgnenoel@gmail.com>
2025-01-30 07:42:33 +0100

    try catch disponibilities

--- app/api/disponibilities/route.ts
+++ app/api/disponibilities/route.ts
@@ -3,4 +3,14 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(request: Request) {}
+export async function GET(request: Request) {
+  try {
+    
+  } catch (error) {
+    console.error("Erreur api disponibilities", error);
+    return NextResponse.json(
+      { message: "Une erreur est survenue lors de la récupération des salles disponibles" },
+      { status: 500 }
+    );
+  }
+}
