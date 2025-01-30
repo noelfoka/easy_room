e2b9976bcbae8c474c2e97057d06662d37b5cb6b
noelfoka <nomgnenoel@gmail.com>
2025-01-30 07:47:00 +0100

    recuperation des données de la requête

--- app/api/disponibilities/route.ts
+++ app/api/disponibilities/route.ts
@@ -3,8 +3,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   try {
+
+    // recuperation des données de la requête
+    const { roomId, reservationDate } = await request.json();
     
   } catch (error) {
     console.error("Erreur api disponibilities", error);
