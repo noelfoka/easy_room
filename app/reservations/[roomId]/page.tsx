8c99258991c7a574ccd68fc4ce8eff48065cfb2a
noelfoka <nomgnenoel@gmail.com>
2025-01-31 09:42:42 +0100

    recupérer l'utilisateur connecté

--- app/reservations/[roomId]/page.tsx
+++ app/reservations/[roomId]/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import Wrapper from '@/app/components/Wrapper'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 const page = ({ params} : { params: { roomId: string } }) => {
+
+  // recupérer l'utilisateur connecté
+  const { user } = useKindeBrowserClient();
+
   return (
    <Wrapper>
     <div>Reservation de la salle {params.roomId}</div>
