3845f79334ee5bc9bb27216587a9976cfce092d5
noelfoka <nomgnenoel@gmail.com>
2025-01-31 09:45:23 +0100

    Les variables d'etat

--- app/reservations/[roomId]/page.tsx
+++ app/reservations/[roomId]/page.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client"
 
 import Wrapper from '@/app/components/Wrapper'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+import React, { useState } from 'react'
 
 const page = ({ params} : { params: { roomId: string } }) => {
 
   // recupérer l'utilisateur connecté
   const { user } = useKindeBrowserClient();
+
+  // Les variables d'etat
+  const [notification, setNotification] = useState<string | null>(null);
+
+  const closeNotification = () => {
+    setNotification(null);
+  };
 
   return (
    <Wrapper>
